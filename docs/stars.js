0510a13fa66c560e0923083f1252112c0a03f42b
leh1551 <leticiatavaresbraga15@gmail.com>
2023-10-25 21:15:12 -0300

    incluindo calculo de total e  botão de sacola

--- docs/stars.js
+++ docs/stars.js
@@ -22,3 +22,41 @@ document.getElementById('submit').addEventListener('click', () => {
     const comment = commentBox.value;
     commentText.innerText = `Seu comentário:${comment}`;
 });
+
+const cartButton = document.getElementById("btn-sacola");
+const cartCount = document.getElementById("cart-count");
+
+let cartItemCount = 0;
+
+// Adicionar um item à sacola
+function addItemToCart() {
+    cartItemCount++;
+    updateCartCount();
+}
+
+// Atualizar a contagem na sacola
+function updateCartCount() {
+    cartCount.textContent = cartItemCount;
+}
+
+// Evento de clique para adicionar um item
+cartButton.addEventListener("click", () => {
+    addItemToCart();
+});
+
+// Inicialização da contagem
+updateCartCount();
+
+
+function calcularTotal() {
+    var quantidade = parseFloat(document.getElementById("quantidade").value);
+    var precoPorItem = parseFloat(document.getElementById("precoPorItem").value);
+
+    var total = quantidade * precoPorItem;
+
+    if (!isNaN(total)) {
+        document.getElementById("total").textContent = "Total: R$ " + total.toFixed(2);
+    } else {
+        document.getElementById("total").textContent = "Total: R$ 0.00";
+    }
+}

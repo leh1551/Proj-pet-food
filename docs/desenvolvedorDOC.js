f619c491c785508694d3e7f5e7a5b8a9dbcc593a
unknown <felipe.bengevenga@gmail.com>
2023-11-28 20:43:02 -0300

    atualizando e adicionando telas

--- docs/desenvolvedorDOC.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const meuElemento = document.querySelector('#meuId');
-    if (meuElemento !== null) {
-      meuElemento.addEventListener('click', minhaFuncao);
-    } else {
-      console.error('Elemento é null.');
-    }
-  });
